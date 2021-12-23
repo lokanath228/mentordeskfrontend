298f1b568e137e647c40c677e01f018223af8dd6
lokanath228 <loguvima228@gmail.com>
2021-12-23 16:38:40 +0530

    first commit

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import { Grid,CircularProgress } from '@material-ui/core';
-import { useSelector } from 'react-redux';
- //import posts from './reducers/posts.js';
-import Post from './Post/Post.js';
+import React from 'react'
+import { Grid, CircularProgress } from '@material-ui/core'
+import { useSelector } from 'react-redux'
+//import posts from './reducers/posts.js';
+import Post from './Post/Post.js'
 
-import useStyles from './style.js';
+import useStyles from './style.js'
 
-const Posts =({setCurrentId}) => {
-    
-    const posts = useSelector((state) => state.posts);
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts)
 
-    const classes= useStyles();
-    
-    return (
-        !posts.length ? <CircularProgress /> : (
-          <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-            {posts.map((post) => (
-              <Grid key={post._id} item xs={12} sm={6} md={6}>
-                <Post post={post} setCurrentId={setCurrentId}   />
-              </Grid>
-            ))}
-          </Grid>
-        )
-      );
-    };
-    
-    export default Posts;
+  const classes = useStyles()
+
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems='stretch'
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={4} sm={4} md={4}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+export default Posts
